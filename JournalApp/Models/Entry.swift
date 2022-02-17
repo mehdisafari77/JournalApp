57b79be4963b67f4853b50bf95a26aeb844498ff
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-02-17 12:21:06 -0800

    add initial files and folder structure for the app

---
+++ JournalApp/Models/Entry.swift
@@ -0,0 +1,8 @@
+//
+//  Entry.swift
+//  JournalApp
+//
+//  Created by Mehdi MMS on 17/02/2022.
+//
+
+import Foundation
---
+++ JournalApp/ViewControllers/EntryController.swift
@@ -0,0 +1,8 @@
+//
+//  EntryController.swift
+//  JournalApp
+//
+//  Created by Mehdi MMS on 17/02/2022.
+//
+
+import Foundation
