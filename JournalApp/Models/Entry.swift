17f71f05dc71a23ee940b5737894d8222cd1b8d8
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-02-17 13:17:05 -0800

    add Entry model

--- JournalApp/Models/Entry.swift
+++ JournalApp/Models/Entry.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+class Entry: Codable {
+    let title: String
+    let body: String
+    
+    init(title: String, body: String) {
+        self.title = title
+        self.body = body
+    }
+}
+
+extension Entry: Equatable {
+    static func == (lhs: Entry, rhs: Entry) -> Bool {
+        return lhs.title == rhs.title && lhs.body == rhs.body
+    }
+}
