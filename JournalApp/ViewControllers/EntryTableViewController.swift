95c0fdda72c6f2253ffd9a7e162b6c7e31383708
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-02-17 13:22:11 -0800

    done with tableviewcontroller code

--- JournalApp/ViewControllers/EntryTableViewController.swift
+++ JournalApp/ViewControllers/EntryTableViewController.swift
@@ -8,82 +8,70 @@
 import UIKit
 
 class EntryTableViewController: UITableViewController {
-
+    
+    @IBOutlet weak var entryTitleTextField: UITextField!
+    @IBOutlet weak var entryBodyTextField: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        EntryController.shared.loadFromPersistenceStore()
     }
-
+    
+    @IBAction func addEntryButton(_ sender: Any) {
+        // must unwrap using guard statement
+        guard let entryTitle = entryTitleTextField.text,
+              let entryBody = entryBodyTextField.text,
+              // check that both required fields aren't empty
+              !entryTitle.isEmpty,
+              !entryBody.isEmpty else { return }
+        // calls createEntry() method in EntryController file
+        EntryController.shared.createEntry(title: entryTitle, body: entryBody)
+        entryTitleTextField.text = ""
+        entryBodyTextField.text = ""
+        tableView.reloadData() // reload table after adding song
+    }
+    
     // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return EntryController.shared.entries.count //get current count of array
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "entryCell", for: indexPath)
 
-        // Configure the cell...
-
+        let entry = EntryController.shared.entries[indexPath.row]
+        
+        cell.textLabel?.text = entry.title
+        cell.detailTextLabel?.text = entry.body
+        
         return cell
     }
-    */
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
+            // get the row that was swiped
+            let entryToDelete = EntryController.shared.entries[indexPath.row]
+            //call deleteEntry() method in EntryController file
+            EntryController.shared.deleteEntry(entry: entryToDelete)
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
     }
-    */
 
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        guard let tappedIndexPath = tableView.indexPathForSelectedRow else { return }
+        let entry = EntryController.shared.entries[tappedIndexPath.row]
+        if let detailVC = segue.destination as? EntryDetailViewController {
+            detailVC.entry = entry
+        }
     }
-    */
+
 
 }
